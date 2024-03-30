9c8841147c265d0aed820aaf7153c8aafaf2c994
CJendantix <75325223+CJendantix@users.noreply.github.com>
2024-03-30 09:53:48 -0400

    Create config-generator.sh

---
+++ config-generator.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+findSuffix () {
+	INPUT="$1";
+	OUTPUT="";
+
+	if [ "${INPUT: -2:1}" = 1 ]; then
+		OUTPUT="th";
+	else
+		case ${INPUT: -1} in
+			1)
+				OUTPUT="st";
+				;;
+			2)
+				OUTPUT="nd";
+				;;
+			3)
+				OUTPUT="rd";
+				;;
+			*)
+				OUTPUT="th";
+				;;
+		esac
+	fi
+
+	echo "$OUTPUT"
+}
+
+read -rp "How many monitors do you have? " TOTALMONITORS;
+read -rp "How many workspaces per monitor do you want " WORKSPACESPERMONITOR;
+MONITORNAMES=()
+
+if [ "$WORKSPACESPERMONITOR" -gt 9 ]; then
+	echo "Cannot create more than nine workspaces per monitor";
+	exit;
+fi
+
+for i in $(seq "$TOTALMONITORS"); do
+	suffix=$(findSuffix "${i}");
+	read -rp "What is the name of the ${i}${suffix} monitor? " x;
+	MONITORNAMES+=("$x");
+done
+
+echo ""
+for x in $(seq "$WORKSPACESPERMONITOR"); do
+		echo "bind = SUPER, ${x}, exec, ${HOME}/.local/share/scripts/workspaces.sh ${x} ${WORKSPACESPERMONITOR}";
+done
+echo ""
+for x in $(seq "$WORKSPACESPERMONITOR"); do
+		echo "bind = SUPER_SHIFT, ${x}, exec, ${HOME}/.local/share/scripts/workspaces-move.sh ${x} ${WORKSPACESPERMONITOR}";
+done
+echo ""
+for x in $(seq 0 "$(( TOTALMONITORS - 1 ))"); do
+	for y in $(seq "$WORKSPACESPERMONITOR"); do
+		default="false"
+		if [ "$y" == 1 ]; then
+			default="true";
+		fi
+		echo "workspace = $(( y + x * WORKSPACESPERMONITOR )),monitor:${MONITORNAMES[$x]},default:${default}";
+	done
+	echo ""
+done
