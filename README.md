3556e03a2685270a6f4cd5b7074b731eebffd9b1
CJ Gilroy <cjendantix@proton.me>
2024-09-01 18:39:17 -0400

    Modified the generator to function with the automatic installer

--- README.md
+++ README.md
@@ -3,8 +3,11 @@ Scripts to make Hyprland workspaces a little more usable with multiple monitors
 
 
 ## How to install
+### Automatic Installation
+Download the `installer.sh` script from the Releases page and run it
+### Manual Installation
 1. Clone the repository
-2. Create the folder `~/.local/share/scripts`
+2. Create the directory `~/.local/share/scripts/hyprland_workspace_scripts/`
 3. Copy all scripts into said directory
 4. Make `workspaces-move.sh` & `workspaces.sh` executable
 5. Run `config-generator.sh` and follow instructions
--- config-generator.sh
+++ config-generator.sh
@@ -26,42 +26,48 @@ findSuffix () {
 	echo "$OUTPUT"
 }
 
-read -rp "How many monitors do you have? " TOTALMONITORS;
-read -rp "How many workspaces per monitor do you want? " WORKSPACESPERMONITOR;
-read -rp "How many workspaces do you want to be persistent per monitor? " PERSISTENTWORKSPACES;
-MONITORNAMES=()
-
-if [ "$WORKSPACESPERMONITOR" -gt 9 ]; then
-	echo "Cannot create more than nine workspaces per monitor";
-	exit;
+if [ ! "$1" ]; then
+	read -rp "How many monitors do you have? " TOTALMONITORS;
+	read -rp "How many workspaces per monitor do you want? " WORKSPACESPERMONITOR;
+	read -rp "How many workspaces do you want to be persistant per monitor? " PERSISTANTWORKSPACES;
+	MONITORNAMES=()
+	
+	if [ "$WORKSPACESPERMONITOR" -gt 9 ]; then
+		echo "Cannot create more than nine workspaces per monitor";
+		exit;
+	fi
+	
+	for i in $(seq "$TOTALMONITORS"); do
+		suffix=$(findSuffix "${i}");
+		read -rp "What is the name of the ${i}${suffix} monitor? " x;
+		MONITORNAMES+=("$x");
+	done
+else
+	TOTALMONITORS="$1"
+	WORKSPACESPERMONITOR="$2"
+	PERSISTANTWORKSPACES="$3"
+	MONITORNAMES=("$4")
 fi
-
-for i in $(seq "$TOTALMONITORS"); do
-	suffix=$(findSuffix "${i}");
-	read -rp "What is the name of the ${i}${suffix} monitor? " x;
-	MONITORNAMES+=("$x");
-done
-
 echo ""
 for x in $(seq "$WORKSPACESPERMONITOR"); do
-		echo "bind = SUPER, ${x}, exec, ${HOME}/.local/share/scripts/workspaces.sh ${x} ${WORKSPACESPERMONITOR}";
+		echo "bind = SUPER, ${x}, exec, ~/.local/share/scripts/hyprland_workspace_scripts/workspaces.sh ${x} ${WORKSPACESPERMONITOR}";
 done
 echo ""
 for x in $(seq "$WORKSPACESPERMONITOR"); do
-		echo "bind = SUPER_SHIFT, ${x}, exec, ${HOME}/.local/share/scripts/workspaces-move.sh ${x} ${WORKSPACESPERMONITOR}";
+		echo "bind = SUPER_SHIFT, ${x}, exec, ~/.local/share/scripts/hyprland_workspace_scripts/workspaces-move.sh ${x} ${WORKSPACESPERMONITOR}";
 done
 echo ""
 for x in $(seq 0 "$(( TOTALMONITORS - 1 ))"); do
 	for y in $(seq "$WORKSPACESPERMONITOR"); do
 		default="false"
-		persistent="false"
+		persistant="false"
 		if [ "$y" == 1 ]; then
 			default="true";
 		fi
-		if [ "$y" -le "$PERSISTENTWORKSPACES" ]; then
-			persistent="true"
+		if [ "$y" -le "$PERSISTANTWORKSPACES" ]; then
+			persistant="true"
 		fi
-		echo "workspace = $(( y + x * WORKSPACESPERMONITOR )),monitor:${MONITORNAMES[$x]},default:${default},persistent:${persistent}";
+		echo "workspace = $(( y + x * WORKSPACESPERMONITOR )),monitor:${MONITORNAMES[$x]},default:${default},persistant:${persistant}";
 	done
 	echo ""
 done
